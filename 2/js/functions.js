4ce12b9d2823bd876bc1b0efb60b24b1c5cf3ebd
Keks <keks@htmlacademy.ru>
2024-06-25 15:54:00 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,28 @@
+//Функция для проверки длины строки
+const checkStringLength = (inputString, maxLength) => (inputString.length <= maxLength) ? 'true' : 'false';
+
+//Функция для проверки, является ли строка палиндромом
+const checkStringIsPalindrome = (inputString) => {
+  inputString = inputString.toLowerCase().replaceAll(' ','');
+  let reversedString = '';
+  for (let i = -1; Math.abs(i) <= inputString.length; i--) {
+    reversedString += inputString.at(i);
+  }
+  return (inputString === reversedString) ? 'true' : 'false';
+};
+
+//Дополнительное задание. Функция извелечения целого положительного числа
+const getNumbers = (inputString) => {
+  inputString = inputString.toString();
+  let result = '';
+  for (let i = 0; i <= inputString.length - 1; i++) {
+    if (!Number.isNaN(parseInt(inputString[i], 10))) {
+      result += inputString[i];
+    }
+  }
+  return (result === '') ? 'NaN' : Math.abs(result);
+};
+
+checkStringLength();
+checkStringIsPalindrome();
+getNumbers();
