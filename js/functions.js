20ebc868e7785ccbf98831b04ac077649abad8eb
Павел Корольков <roflgan@gmail.com>
2024-07-11 21:47:19 +0700

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -26,3 +26,16 @@ const getNumbers = (inputString) => {
 checkStringLength('tester', 10);
 checkStringIsPalindrome('довод');
 getNumbers('fdhsfkj 032$*#!JDSAL!0-00');
+
+//5.16. Функции возвращаются
+
+const getMinutes = (time) => parseInt(time.split(':')[0], 10) * 60 + parseInt(time.split(':')[1], 10);
+
+const isMeetingWithinWorkingDay = (timeStartOfTheDay, timeEndOfTheDay, timeStartOfTheMeeting, duratationOfMeeting) =>
+  !((getMinutes(timeStartOfTheDay) > getMinutes(timeStartOfTheMeeting)) || ((getMinutes(timeStartOfTheMeeting) + duratationOfMeeting) > getMinutes(timeEndOfTheDay)));
+
+isMeetingWithinWorkingDay('08:00', '17:30', '14:00', 90); // true
+isMeetingWithinWorkingDay('8:0', '10:0', '8:0', 120); // true
+isMeetingWithinWorkingDay('08:00', '14:30', '14:00', 90); // false
+isMeetingWithinWorkingDay('14:00', '17:30', '08:0', 90); // false
+isMeetingWithinWorkingDay('8:00', '17:30', '08:00', 900); // false
