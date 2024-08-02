25c12d2b6ae5d5424f73e134c8b2c542011b9b34
Павел Корольков <roflgan@gmail.com>
2024-08-02 11:07:57 +0700

    переход на данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-import './thumbnails.js';
-import './expanded-post-events.js';
 import './form-events.js';
 import './form-validator.js';
 import './form-scale.js';
+import { getData } from './api.js';
+import { renderPosts } from './thumbnails.js';
+import { setUsersPosts } from './expanded-post-data.js';
+
+getData()
+  .then(
+    (posts) => {
+      renderPosts(posts);
+      setUsersPosts(posts);
+    })
+  .catch(() => {
+    showAlert();
+  });
+
+
